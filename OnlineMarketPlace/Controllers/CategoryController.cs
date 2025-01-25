2cd674bdc781e4017696fa29c0108459e049e4cf
LougLynx <longtd204@gmail.com>
2025-01-25 23:01:11 +0700

    Cập nhật sản phẩm products

---
+++ OnlineMarketPlace/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineMarketPlace.Models;
+using OnlineMarketPlace.Repository;
+
+namespace OnlineMarketPlace.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ProductRepository _productRepository = new();
+        private readonly CategoryRepository _categoryRepository = new();
+
+        public async Task<IActionResult> Index()
+        {
+            var categoriesParent = _categoryRepository.GetCatgoryParent();
+            var categoriesChildList = categoriesParent
+                .Select(parent => _categoryRepository.GetCatgoryChild(parent.Id))
+                .ToList();
+
+            CategoriesList categoriesList = new CategoriesList(categoriesParent, categoriesChildList);
+            ViewData["CategoriesList"] = categoriesList;
+
+            var products = await _productRepository.GetProductsAsync();
+            ViewData["Products"] = products;
+
+            return View();
+        }
+    }
+}
